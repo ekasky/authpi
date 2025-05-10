71ea441bde49255e56f8e20757d158b4199a0a3d
Evan Kasky <ekasky25@gmail.com>
2025-05-09 21:28:50 -0400

    Adds unit tests for postgreSQL DB connection

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   testEnvironment: "node",
   setupFiles: ['<rootDir>/tests/setup/load-env.ts'],
+  // Only collect coverage from source files
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
   },
